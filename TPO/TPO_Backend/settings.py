d9f72a8c24e721d22f18a9a5a8ffe5cf5839971a
Mahmoud-Hijjeh <mahmoud2001asoom@gmail.com>
2022-12-02 19:12:32 +0200

    remove .env file

--- TPO/TPO_Backend/settings.py
+++ TPO/TPO_Backend/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-f^9s!de(=cdlb6uec3er(e_eb^i4kyq0l)c#3f93-khrd@jk4p'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',cast=bool)
+DEBUG = 'True'
 
 ALLOWED_HOSTS = []
 
