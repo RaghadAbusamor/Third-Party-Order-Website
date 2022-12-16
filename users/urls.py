710d181afcb84fd29da921829ee5604f252410a2
RaghadAbuSamor <raghadmohammad253@gmail.com>
2022-12-16 14:29:12 +0200

    add sign up page

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path,include
-from users.views import dashboard,register,CustomerSignUpView , about#,SignUpView,StoreSignUpView,DeliverySignUpView
+from users.views import dashboard,register,CustomerSignUpView, about#,SignUpView,StoreSignUpView,DeliverySignUpView
 urlpatterns = [
     path("", dashboard, name="dashboard"),
-    path('accounts/signup/customer/', CustomerSignUpView, name='customer_signup'),
+    path('accounts/signup/customer/', CustomerSignUpView, name="CustomerSignUpView"),
     path("accounts/", include("django.contrib.auth.urls")),
     #path('oauth/', include('social_django.urls', namespace="social")),
     #path('accounts/signup/', SignUpView.as_view(), name='signup'),
@@ -10,5 +10,5 @@ urlpatterns = [
     #path('accounts/signup/store/', StoreSignUpView.as_view(), name='store_signup'),
     #path('accounts/signup/delivery/', DeliverySignUpView.as_view(), name='delivery_signup'),
     path("register/", register, name="register"),
-    path("about",about, name="about"),
+    path("about/",about, name="about"),
 ]
